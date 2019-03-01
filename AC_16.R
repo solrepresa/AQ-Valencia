f30770a0a331e6675f2bfe3a1888def6fb928b94
Sol Represa <solrepresa@users.noreply.github.com>
2019-03-01 17:26:52 +0100

    Add files via upload

--- AC_16.R
+++ AC_16.R
@@ -50,40 +50,53 @@ raster_template <- raster(nrows = 239, ncols = 158, #1 km de resolucion aprox
                           ext = extent(MCD19A2))  # toma las extensiones
 
 
+sds_error <- list() # junto file que dan error
 
 mapply(file = id,
        FUN = function(file){
-         sds <- get_subdatasets(file) #lista de nombres de las SDS 
-         
-         for( j in 1:length(sds)){
-           name_sds <- sds[j]
-           filename <- paste(substr(name_sds, 19, 54), substr(name_sds, 71, nchar(name_sds)), ".tif", sep="")
-           gdal_translate(name_sds, dst_dataset = filename)
-           MIRRAraster <- raster(filename)
+         tryCatch(
+           { #si no sale error quiero que haga todo lo siguiente:
+             sds <- get_subdatasets(file) #lista de nombres de las SDS 
+             
+             for( j in 1:length(sds)){
+               name_sds <- sds[j]
+               filename <- paste(substr(name_sds, 19, 54), substr(name_sds, 71, nchar(name_sds)), ".tif", sep="")
+               gdal_translate(name_sds, dst_dataset = filename)
+               MIRRAraster <- raster(filename)
+               
+               # 2) Reproyectar
+               MIRRAraster <- projectRaster(MIRRAraster,
+                                            crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ",
+                                            method = "bilinear")
+               
+               # 3)Recortar
+               data_recorte <- crop(MIRRAraster, shape_trans)  #recorto imagen para Valencia
+               
+               # 4) Guardar recorte
+               writeRaster(data_recorte, paste(getwd(), "\\raster\\", filename, sep = ""), format = "GTiff")
+
+               # 5) Resampling
+               rst_resampling <- raster::resample(data_recorte, raster_template)
+               
+               # 6) Guardar resampling
+               writeRaster(rst_resampling, paste(getwd(), "\\raster_res\\", filename, sep = ""), format = "GTiff")
+
+               rm(data_recorte, MIRRAraster, rst_resampling)
+             }
+           },
            
-           # 2) Reproyectar
-           MIRRAraster <- projectRaster(MIRRAraster,
-                                        crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ",
-                                        method = "bilinear")
-          
-           # 3)Recortar
-           data_recorte <- crop(MIRRAraster, shape_trans)  #recorto imagen para Valencia
-          
-           # 4) Guardar recorte
-           writeRaster(data_recorte, paste(getwd(), "\\raster\\", filename, sep = ""), format = "GTiff")
-           
-           
-           # 5) Resampling
-           rst_resampling <- raster::resample(data_recorte, raster_template)
-           
-           # 6) Guardar resampling
-           writeRaster(rst_resampling, paste(getwd(), "\\raster_res\\", filename, sep = ""), format = "GTiff")
-           
-           
-           rm(data_recorte, MIRRAraster, rst_resampling)
-         }
+           error = function(error_message){
+             message("Posible error en get_subdatasets(). Esto ocurre cuando hay solo 1 SDS")
+             message(error_message)
+             sds_error[[k]] <- file
+             k = k + 1
+           }
+         )
       }
 )
+
+
+
 
 
 
@@ -95,60 +108,23 @@ mapply(file = id,
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-### CUando en el .hdf solo tiene una unica SDS
+## Cuando en el .hdf solo tiene una unica SDS
+## get_subdatasets() da error
 
+setwd("C:\\Users\\narep\\Desktop\\MERRA") #fundamental para q funcione gdal!
+#id <- dir(pattern = ".hdf") 
 
-setwd("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\variables\\MERRA\\otros\\a") #fundamental paa q funcione gdal!
-id <- dir(pattern = ".hdf") 
-
+id <- list.files(path = getwd(),
+                 pattern = "*.hdf",
+                 full.names = FALSE)
 
 ## Shape recorte
 
 shape <- readOGR("C:\\Users\\narep\\Desktop\\SOL\\AQ-Valencia\\mapa\\valencia.shp")
 shape_trans <- spTransform(shape, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
 
-SDS = "H1000"
 
-for( i in 1: length(id)){
-  # 1) Abrir
-  filename <- paste(substring(id[i], 0, 36), SDS, ".tif", sep="") 
-  
-  gdal_translate(id[i], dst_dataset = filename)
-  MIRRAraster <- raster(filename)
-  
-  # 2) Reproyectar
-  MIRRAraster <- projectRaster(MIRRAraster, 
-                               crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ",
-                               method = "bilinear")
-  
-  # 3)Recortar
-  data_recorte <- crop(MIRRAraster, shape_trans)  #recorto imagen para Valencia
-  
-  # 4) Guardar recorte
-  writeRaster(data_recorte, paste(getwd(), "\\raster\\", filename, sep = ""), format = "GTiff")
-  
-  # 5) Resampling
-  rst_resampling <- raster::resample(data_recorte, raster_template)
-  
-  # 6) Guardar resampling
-  writeRaster(rst_resampling, paste(getwd(), "\\raster_res\\", filename, sep = ""), format = "GTiff")
-  
-  rm(data_recorte, MIRRAraster, rst_resampling)
-
-}
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-## RESEAMPLING  ####
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-# Lista de raster SDS MERRA
-id_MERRA <- dir("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\variables\\MERRA\\raster", pattern = ".tif")
-
-
+# Para resampling
 # Uso imagen MODIS MCD19A2 como modelo para crear raster
 MCD19A2 <- raster("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\MODIS\\crop_res\\MCD19A2.A2008-01-01.h17v04.tif")
 
@@ -157,18 +133,48 @@ raster_template <- raster(nrows = 239, ncols = 158, #1 km de resolucion aprox
                           ext = extent(MCD19A2))  # toma las extensiones
 
 
-
-for( i in 1:length(id_MERRA)){
-  rst <- raster(paste("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\variables\\MERRA\\raster\\", id_MERRA[i], sep = ""))
-  rst_resampling <- raster::resample(rst, raster_template)
-  writeRaster(rst_resampling, paste("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\variables\\MERRA\\raster_res\\", id_MERRA[i], sep = ""), format = "GTiff")
-  
-}
+SDS = "H1000"
 
 
 
+mapply(file = id,
+       FUN = function(file){
+         tryCatch(
+           { #si no sale error quiero que haga todo lo siguiente:
+             
+             filename <- paste(substr(file, 0, 36), SDS, ".tif", sep="")
+             gdal_translate(file, dst_dataset = filename)
+             MIRRAraster <- raster(filename)
+             
+             # 2) Reproyectar
+             MIRRAraster <- projectRaster(MIRRAraster,
+                                          crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ",
+                                          method = "bilinear")
+             
+             # 3)Recortar
+             data_recorte <- crop(MIRRAraster, shape_trans)  #recorto imagen para Valencia
+             
+             # 4) Guardar recorte
+             writeRaster(data_recorte, paste(getwd(), "\\raster\\", filename, sep = ""), format = "GTiff")
+             
+             # 5) Resampling
+             rst_resampling <- raster::resample(data_recorte, raster_template)
+             
+             # 6) Guardar resampling
+             writeRaster(rst_resampling, paste(getwd(), "\\raster_res\\", filename, sep = ""), format = "GTiff")
+             
+             rm(data_recorte, MIRRAraster, rst_resampling)
 
+           },
+           
+           error = function(error_message){
+             message("ATENTI")
+             message(error_message)
 
+           }
+         )
+       }
+)
 
 
 
@@ -188,5 +194,4 @@ SDS[[2]] <- "H1000"
 SDS[[3]] <- c("PBLH" , "SPEED", "SPEEDMAX", "USTAR", "PRECTOT")
 SDS[[4]] <- c("ALBEDO", "CLDHGH", "CLDLOW")
 SDS[[5]] <- c("PS", "T", "RH", "U", "V")
-
 
---
+++ AC_25.R
@@ -0,0 +1,127 @@
+### Calidad del Aire en Comunitat Valenciana
+### 01/03/2019 Valencia, Spain
+### Sol Represa
+### Archivo 25
+
+
+
+### Objetivo: Revisar archivos MERRA generados
+# para ver si tengo las series completas de todas las variables que me interesan
+
+
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+## MISSING de la serie de tiempo
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# Los files procesados
+
+setwd("C:\\Users\\narep\\Desktop\\SOL\\aire_comunitat\\variables\\MERRA\\raster_res") 
+
+id <- list.files(path = getwd(),
+                 pattern = "*.tif",
+                 full.names = FALSE)
+
+
+archivo <- substr(id, 12, 26)
+fecha <- substr(id, 28, 35)
+SDS <- substr(id, 37, nchar(id)-4)
+
+base_MERRA <- data.frame(archivo, fecha, SDS)
+
+base_MERRA$fecha <- as.Date(base_MERRA$fecha, format = "%Y%m%d", tz="GMT" )
+
+datos <- base_MERRA %>% group_by(archivo, fecha) %>% summarise()
+
+datos <- as.data.frame(datos)
+datos <- dcast(datos, fecha ~ archivo)
+
+
+# Completo la serie de tiempo
+fecha <- as.data.frame(seq.Date(as.Date(ISOdate(2008,01,01)), as.Date(ISOdate(2018,12,30)), by = "day", tz = "GMT")) 
+names(fecha) <- "fecha"
+
+missing_data <- merge(datos, fecha, by = "fecha", all = TRUE)
+
+
+
+## Funcion de datos Missing  ####
+
+# Me genera con una tabla donde
+# 0 = NA
+# 1 = hay dato
+
+miss <- function(tabla, date){
+  miss <- data.frame(date) #creo un data.frame con fechas
+  for(j in 2:length(tabla)){ #recorro todas las columnas j de datos
+    for(i in 1:nrow(tabla)){ #recorro todas las filas i
+      if(is.na(tabla[i,j])){
+        miss[i,j] <- 0
+      }else{
+        miss[i,j] <- 1
+      }
+    }
+  }
+  names(miss) <- names(tabla)
+  return(miss)
+}
+
+miss_data <- miss(missing_data, missing_data$fecha)
+miss_data <- melt(miss_data, id.vars = "fecha" )
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Plot para visualizar los datos que tengo procesados ####
+
+
+# plot barras
+
+ggplot(miss_data, aes(x = fecha, y = value)) + 
+  geom_bar(stat="identity") + 
+  theme_bw() + ylab("") + 
+  scale_y_continuous(breaks =  c(0, 1)) + 
+  theme(axis.text.y = element_text(colour="white")) +
+  facet_wrap(~variable)
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+## Lista de archivos que faltan
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+a <- miss_data %>% filter( value == "0")
+
+a$value = 1
+
+ggplot(a, aes( x = fecha, y = value)) + geom_point() +
+  theme(axis.text.y = element_text(colour="white")) +
+  facet_wrap(~variable)
+
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Variables de interes de MERRA
+
+SDS <- vector( mode = "list", length = 5)
+names(SDS) <- c("tavg1_2d_aer_Nx", "tavg1_2d_slv_Nx", "tavg1_2d_flx_Nx", "avg1_2d_rad_Nx", "inst3_3d_asm_Np" )
+
+SDS[[1]] <- c("BCSMASS", "DMSSMASS", "DUSMASS", "DUSMASS25", "OCSMASS", "SO2SMASS", "SO4SMASS", "SSSMASS", "SSSMASS25")
+SDS[[2]] <- "H1000" 
+SDS[[3]] <- c("PBLH" , "SPEED", "SPEEDMAX", "USTAR", "PRECTOT")
+SDS[[4]] <- c("ALBEDO", "CLDHGH", "CLDLOW")
+SDS[[5]] <- c("PS", "T", "RH", "U", "V")
