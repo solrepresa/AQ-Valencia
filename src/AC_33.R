c15b5a70ad699b0f66cd79c81137d4ef30ae8d35
Sol Represa <solrepresa@users.noreply.github.com>
2019-07-09 10:57:15 -0300

    Add files via upload

--- src/AC_33.R
+++ src/AC_33.R
@@ -16,79 +16,95 @@ library(gdata)
 library(maptools)
 library(dismo) #kfold
 library(caret)
+library(parallel)
 
 # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# 1- IDW
+# 1- Obtener puntos de raster
 
 # # # # # # # # # # # # # # # # # # # # # # # # #
 
-
-dir = "modelo_rf_100/salida/"
-dir_salida = "modelo_rf_100/IDW/"
+dir = "stack/diarios/"
+dir_salida = "stack/IDW/"
 crs_project = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
+fs <- list.files(path = dir, 
+                 pattern = "tif",
+                 full.names = FALSE)
+
+# Crear raster template >>> spatial Pixels DF
+r <- raster(paste(dir, fs[1], sep=""))
+raster_template <- raster(nrows = 239, ncols = 158, #100m de resolucion aprox
+                          crs = crs_project, 
+                          ext = extent(r))  # toma las extensiones
+idw.grid <- rasterToPoints(raster_template, spatial = TRUE)
+gridded(idw.grid) <- TRUE   #SpatialPixelsDataFrame
+rm(r)
 
 shape <- readShapePoly("/home/usuario/Sol/aire_comunitat/mapa/valencia_4326.shp",
                        proj4string = CRS(crs_project))
 
 
-fs <- list.files(path = dir, 
-                 pattern = "tif")
-
 RMSE_IDW <- data.frame()
+kfold = 5 # numero de k-fold cross validation
 
-for( i in 1:length(fs)){
-  r <- raster(paste(dir, fs[1], sep=""))
-  filename <- paste("IDW-", fs[1], sep="")
-  pp <- as.data.frame(rasterToPoints(r))
-  coordinates(pp) <- ~x+y
-  #pp@proj4string
-  projection(pp) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-  names(pp)[1] <- "PM25"
-  
 
-  # Crear raster template >>> spatial Pixels DF
-  raster_template <- raster(nrows = 239, ncols = 158, #100m de resolucion aprox
-                            crs = crs_project, 
-                            ext = extent(r))  # toma las extensiones
-  idw.grid <- rasterToPoints(raster_template, spatial = TRUE)
-  gridded(idw.grid) <- TRUE   #SpatialPixelsDataFrame
-  
-  rm(r)
-  
-  # 5-fold cross-validation
-  set.seed(513)
-  kf <- kfold(nrow(pp), k =5)
-  rmse <- rep(NA, 5)
-  for (k in 1:5) {
-    test <- pp[kf == k, ]
-    train <- pp[kf != k, ]
-    kat.idw <- gstat::idw(PM25 ~ 1, train, idw.grid, 
-                          debug.level = -1,   #para ver grado de progreso
-                          idp = 5) # IDW power
 
-    final.idw <- raster(kat.idw)
-    t <- SpatialPoints(test, proj4string = CRS(crs_project)) #convierto formato para aplicar extract
-    #plot(final.idw)
-    model <- extract(final.idw, t)
-    rmse[k] <- caret::RMSE(test$PM25, model)
-  }
- 
-  tabla <- data.frame(archivo = filename, RMSE = mean(rmse, na.rm = TRUE))
-  RMSE_IDW <- rbind(RMSE_IDW, tabla)
+# Para mejorar la performance, paralelizar el proceso!
+cl <- makePSOCKcluster(3)
+registerDoParallel(cl) #registro del proceso paralelo
+
+for( i in 663:length(fs)){
+  print(i)
+  raster_fs <- raster(paste(dir, fs[i], sep=""))
+  filename <- paste("IDW-", fs[i], sep="")
+  raster_points <- as.data.frame(rasterToPoints(r))
+  rm(raster_fs)
+  
+  if(nrow(raster_points) > 3776){  #### control: raster con un 10% de datos
+    coordinates(raster_points) <- ~x+y
+    #raster_points@proj4string
+    projection(raster_points) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    names(raster_points)[1] <- "AOD"
+
+    set.seed(513)  # 5-fold cross-validation
     
-  # Recorto el ultimo armado para valencia
-  data_recorte <- crop(final.idw, shape)  
-  data_recorte <- mask(data_recorte, shape)  
-  
-  # Guardar
-  writeRaster(data_recorte, paste(dir_salida, filename, sep = ""), 
-              format = "GTiff",
-              overwrite = TRUE)
-  
-  rm(data_recorte, raster_template, train, test, idw.grid, kat.idw)
+    kf <- kfold(nrow(raster_points), k = kfold)
+    rmse <- rep(NA, kfold)
+    for (k in 1:kfold) {   
+      test <- raster_points[kf == k, ]
+      train <- raster_points[kf != k, ]
+      kat.idw <- gstat::idw(AOD ~ 1, train, idw.grid, 
+                            debug.level = -1,   #para ver grado de progreso
+                            idp = 5) # IDW power
+      
+      final.idw <- raster(kat.idw)
+      t <- SpatialPoints(test, proj4string = CRS(crs_project)) #convierto formato para aplicar extract
+      #plot(final.idw)
+      model <- extract(final.idw, t)
+      rmse[k] <- caret::RMSE(test$AOD, model)
+    }
+    
+    tabla <- data.frame(archivo = filename, RMSE = mean(rmse, na.rm = TRUE))
+    RMSE_IDW <- rbind(RMSE_IDW, tabla)
+    
+    # Recorto el ultimo armado para valencia
+    data_recorte <- crop(final.idw, shape)  
+    data_recorte <- mask(data_recorte, shape)  
+    
+    # Guardar
+    writeRaster(data_recorte, paste(dir_salida, filename, sep = ""), 
+                format = "GTiff",
+                overwrite = TRUE)
+    
+    rm(data_recorte, raster_template, train, test, idw.grid, kat.idw)
+    
+  }
   
 }
 
-write.csv(RMSE_IDW, "error_IDW.csv" , row.names = FALSE)
+stopCluster(cl) #cerrar
+remove(cl)
+registerDoSEQ()
+
+write.csv(RMSE_IDW, "error_IDW_diarios_aod.csv" , row.names = FALSE)
