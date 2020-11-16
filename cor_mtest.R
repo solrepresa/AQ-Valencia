1732eab4542b64bb9429d7a89e9a4611b52acd12
Sol Represa <solrepresa@users.noreply.github.com>
2020-11-16 13:00:57 -0300

    Add files via upload

---
+++ cor_mtest.R
@@ -0,0 +1,21 @@
+
+library('corrplot')
+
+# Tabla con p-value 
+
+# para incorporar a grafica de correlacion
+# Sacado de: http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
+cor_mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat) 
+  p.mat
+}
